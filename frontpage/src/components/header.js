14bb300af072d80eaf9d4c0558c300ec9014deba
Octavia Suceava <72067315+917OctaviaSuceava@users.noreply.github.com>
2023-06-11 14:12:25 +0300

    header

--- frontpage/src/components/header.js
+++ frontpage/src/components/header.js
@@ -4,10 +4,15 @@ import './Header.css';
 function Header() {
   return (
     <div class="topnav">
+        <a class="title" href="#">
+            <img src={require('./logobook.png')} alt="Logo" class="logo"/>
+            <span class="title-text">Fake News Detection</span>
+        </a>
+        
         <a href="#about">About us</a>
         <a href="#help">Help</a>
         <a href="#home">Home</a>
-  </div>
+    </div>
   );
 }
 
